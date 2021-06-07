d34a5a4e9c18e43b794b8d9a5af2ec803d3f9a92
theGloves <yusfko@hotmail.com>
2021-06-07 17:04:52 +0800

    [feat](state) 实现state executer的接口

--- state/executor.go
+++ state/executor.go
@@ -5,15 +5,16 @@ import (
 	"chainbft_demo/store"
 	"chainbft_demo/types"
 	"github.com/tendermint/tendermint/libs/log"
+	"time"
 )
 
 type BlockExecutor interface {
 	// CreateProposal 从mempool按照交易到达的顺序打包交易
 	// 需要注意打包的交易不能和pre-commit区块的任意一个交易有冲突
-	CreateProposal([]*types.Block) *types.Proposal
+	CreateProposal(State, types.LTime) *types.Proposal
 
 	// Apply一个指定的区块，如果提交成功后state发生变化，返回新的state
-	ApplyBlock(block *types.Block) (State, error)
+	ApplyBlock(state State, block *types.Block) (State, error)
 
 	SetLogger(logger log.Logger)
 }
@@ -40,15 +41,86 @@ func (exec *blockExcutor) SetLogger(logger log.Logger) {
 }
 
 // ApplyBlock implements BlockExecutor
-func (exec *blockExcutor) ApplyBlock(block *types.Block) (State, error) {
-	// TODO apply上一轮slot的proposal，同时会尝试更新pre-commit区块
-	return State{}, nil
+// apply上一轮slot的proposal，同时会尝试更新pre-commit区块
+func (exec *blockExcutor) ApplyBlock(state State, block *types.Block) (State, error) {
+	// 首先验证区块是否合法，不合法直接返回愿状态
+	if err := exec.validateBlock(state, block); err != nil {
+		return state, ErrInvalidBlock(err)
+	}
+
+	// 决定哪些区块可以提交
+	toCommitBlock := state.decideCommitBlocks(block)
+
+	// 正式提交交易，在这里可能不止提交一个交易，如果提交成功后还要负责删除mempool中的交易
+	state, err := exec.Commit(state, toCommitBlock)
+	if err != nil {
+		return state, err
+	}
+
+	// 更新state的状态
+	_ = state.BlockTree.AddBlocks(block.LastBlockHash, block)
+	state.LastBlockTime = time.Now()
+	state.LastBlockHash = block.BlockHash
+	state.LastBlockSlot = block.Slot
+
+	return state, err
 }
 
-func (exec *blockExcutor) CreateProposal(pendingBlocks []*types.Block) *types.Proposal {
+// 按照list的顺序提交多个区块，是否要保证原子性待定
+// 如果提交成功后更新mempool
+func (exec *blockExcutor) Commit(state State, toCommitblocks []*types.Block) (State, error) {
+	toRemovesTxs := types.Txs{}
+	// 假设都能全部提交成功
+	for idx, block := range toCommitblocks {
+		exec.logger.Debug("commit block", "state", state, "idx", idx)
+		for _, tx := range block.Txs {
+			// commit tx
+			exec.logger.Debug(
+				"commit tx",
+				"tx", tx)
+		}
+		// TODO 检查交易执行的状态 如果有一个区块交易执行失败，直接结束本轮提交，是否需要rollback待定
+		block.BlockState = types.CommiitedBlock
+		//block.ResultHash = newhash
+		toRemovesTxs.Append(block.Txs)
+	}
 
+	// 提交成功后更新mempool，首先加锁
+	exec.mempool.Lock()
+	exec.mempool.Update(0, toRemovesTxs)
+	exec.mempool.Unlock()
+
+	newState := state.Copy()
+	// 更新precommit blocks
+	newState.CommitBlocks(toCommitblocks)
+	// TODO 更新提交后的merkle root
+	newState.LastResultsHash = newState.LastResultsHash
+
+	return newState, nil
+}
+
+// 从mempool中打包交易
+func (exec *blockExcutor) CreateProposal(state State, nextSlot types.LTime) *types.Proposal {
 	// reap all tx from mempool
 	// 需要变更mempool的接口，reap时候要选择没有冲突的交易
-	_ = exec.mempool.ReapMaxTxs(-1)
+	txs := exec.mempool.ReapMaxTxs(-1)
+	block := types.MakeBlock(state.ChainID, nextSlot, txs)
+	return &types.Proposal{
+		ChainID: state.ChainID,
+		Slot:    nextSlot,
+		Block:   block,
+	}
+}
+
+// 根绝当前的state验证一个区块是否合法
+// TODO
+func (exec *blockExcutor) validateBlock(state State, block *types.Block) error {
+	// 先检验区块基本的信息是否正确
+	if err := block.ValidteBasic(); err != nil {
+		return err
+	}
+
+	// TODO 检验block是否符合state
+
 	return nil
 }
--- state/state.go
+++ state/state.go
@@ -1,11 +1,75 @@
 package state
 
-func NewState() State {
-	return State{}
+import (
+	"chainbft_demo/types"
+	"time"
+)
+
+func NewState(
+	chainID string,
+	InitialSlot types.LTime) State {
+	return State{
+		ChainID:     chainID,
+		InitialSlot: InitialSlot,
+	}
 }
 
 // 有限确定状态机的一个状态节点
 // 节点里应维护所有和区块相关的信息
 // 理论上一次共识完成后一定会使状态节点发生
 type State struct {
+	// 初始设定值 const value
+	ChainID     string
+	InitialSlot types.LTime // 初始Slot
+
+	// 最后提交的区块的信息
+	LastBlockSlot types.LTime
+	LastBlockHash []byte
+	LastBlockTime time.Time // 提交的时间 - 物理时间
+
+	// uncommitted blocks
+	// TODO 查询操作的比重会很大 - 能在PreCommitBlocks快速找到blockhash对应的区块
+	PreCommitBlocks []*types.Block
+	SuspectBlocks   []*types.Block
+
+	// block tree - 所有收到非error区块组织形成的树，根节点一定是genesis block
+	BlockTree *types.BlockTree
+
+	// 最后提交区块的结果集的hash or merkle root？
+	LastResultsHash []byte
+}
+
+// 返回当前state的拷贝副本，deepcopy
+func (state *State) Copy() State {
+	newState := State{
+		ChainID:         state.ChainID,
+		InitialSlot:     state.InitialSlot,
+		LastBlockSlot:   state.LastBlockSlot,
+		LastBlockHash:   make([]byte, len(state.LastBlockHash)),
+		LastBlockTime:   state.LastBlockTime,
+		PreCommitBlocks: make([]*types.Block, len(state.PreCommitBlocks)),
+		SuspectBlocks:   make([]*types.Block, len(state.SuspectBlocks)),
+		BlockTree:       &types.BlockTree{},
+		LastResultsHash: make([]byte, len(state.LastResultsHash)),
+	}
+
+	copy(newState.LastBlockHash, state.LastBlockHash)
+
+	return newState
+}
+
+// 遵循正常扩展分支的扩展逻辑，返回一个新的区块应该follow的区块
+func (state *State) NewBranch() *types.Block {
+	return nil
+}
+
+func (state *State) CommitBlocks(blocks []*types.Block) {
+	for _, _ = range blocks {
+		// TODO  把block从state中的PreCommitBlocks和SuspectBlocks移除
+	}
+}
+
+// 在当前状态，根据新的block给出可以提交的区块
+func (state *State) decideCommitBlocks(block *types.Block) []*types.Block {
+	return state.PreCommitBlocks
 }
