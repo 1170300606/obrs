18cb0a66c4e55379add6cd12a91967c4e994d866
theGloves <yusfko@hotmail.com>
2021-11-10 16:15:36 +0800

    DOCKER 加入small bank test database

--- DOCKER/build.sh
+++ DOCKER/build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 BINARY=chain_bft
 
 rm -f $BINARY
 cd ..
+
+# init small bank database
+make build
+./build/$BINARY init-db --account-sum 1000 --dir ./build
+
 make build-linux
 cd DOCKER
 cp ../build/$BINARY ./
+cp -R ../build/smallbank.db ./
 
 docker build -t "chain_bft:latest" .
+
+rm -rf ./smallbank.db
